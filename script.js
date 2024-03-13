d5a3926629655a16b0ca4c44b5cb84ef9eccdd20
ultrabot <72345759+ishakson@users.noreply.github.com>
2024-03-13 10:13:11 +0000

    main

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
- 
+
+let country = 'Portugal';
+let continent = 'Europe';
+let population = 10;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+
+console.log(typeof country);
+console.log(typeof language);
