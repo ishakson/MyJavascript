51e033c561fc38db9ab769571daf8d0e7223e27b
ultrabot <72345759+ishakson@users.noreply.github.com>
2024-03-14 09:02:42 +0000

    main

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
+ const continent = "asia";
+const country = "turkey";
+const language = "turkish";
+let population = 85;
 
-let country = 'Portugal';
-let continent = 'Europe';
-let population = 10;
+console.log(population / 2);
+population++;
+console.log(population > 6);
+console.log(population < 33);
 
-console.log(country);
-console.log(continent);
-console.log(population);
-
-let isIsland = false;
-let language;
-
-console.log(typeof isIsland);
-console.log(typeof population);
-
-console.log(typeof country);
-console.log(typeof language);
+console.log(`${country} is in ${continent}, and it's ${population} million people speak ${language}`);
