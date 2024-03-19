68ba0ac8418edd46a8c37af156da0874091c82d2
ultrabot <72345759+ishakson@users.noreply.github.com>
2024-03-19 09:47:57 +0000

    main

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-age = 19;
-console.log(`I like to drink ${age > 18 ? 'wine' : 'water'}.`);
+ 'use strict';
+
+
+const cutFruitPieces = function(fruit){
+   
+    return fruit * 4
+}
+const fruitProcessor = function(fruit){
+   const pieces = cutFruitPieces(fruit);
+   return pieces;
+}
+
+
+console.log(fruitProcessor(2))
