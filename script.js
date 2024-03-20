60346b3195a272b161585c479f2073bcd52816b5
ishak <ishaksylmz7@gmail.com>
2024-03-20 14:25:37 +0300

    main

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
  'use strict';
 
+ const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
+ let tips = []
+ let totals = []
 
-const cutFruitPieces = function(fruit){
-   
-    return fruit * 4
-}
-const fruitProcessor = function(fruit){
-   const pieces = cutFruitPieces(fruit);
-   return pieces;
+function calcTip(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
 
+function calcTotal(bill, tip) {
+    return bill + tip;
+}
 
-console.log(fruitProcessor(2))
+tips = bills.map(calcTip);
+totals = bills.map((bill , index)=> calcTotal(bill, tips[index]))
+
+console.log(bills)
+console.log(tips)
+console.log(totals)
