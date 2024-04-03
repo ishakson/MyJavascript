b183e77cc7b92df5bd37a113ec879713958b91dc
ultrabot <72345759+ishakson@users.noreply.github.com>
2024-04-03 05:36:52 +0000

    main

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const buttons = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", () => {
+      modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+    })
+}
+
+
+
+closeModal.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+})
+
+
+document.addEventListener('keydown', (e) => {
+  if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+});
+
+
+overlay.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+})
